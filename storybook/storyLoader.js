09ddd60e2fa7713df7a4f4922d8bb6120a857dbb
Shad Mirza <iamshadmirza@gmail.com>
2019-11-27 14:22:06 +0530

    Storybook configured

---
+++ storybook/storyLoader.js
@@ -0,0 +1,17 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('../react-native-design-system/src/Button/Button.stories');
+}
+
+const stories = [
+	'../react-native-design-system/src/Button/Button.stories'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
