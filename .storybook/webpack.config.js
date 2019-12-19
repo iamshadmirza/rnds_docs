f8b8e079c0d060b7b0a6e1571117a2b6c58a3b13
Shad Mirza <iamshadmirza@gmail.com>
2019-12-19 22:39:48 +0530

    ttf files added

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -13,7 +13,7 @@ const babelWorkerOptions = {
 
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
-    test: /\.jsx?$/,
+    test: /\.(jsx|js)?$/,
     include: [
       path.resolve(__dirname, "../node_modules/react-native"),
       path.resolve(__dirname, "../node_modules/react-native-vector-icons"),
@@ -32,8 +32,15 @@ module.exports = ({ config, mode }) => {
       }
     ]
   });
+
+  config.module.rules.push({
+    test: /\.ttf$/,
+    loader: "url-loader", // or directly file-loader
+    include: path.resolve(__dirname, "../", "node_modules/react-native-vector-icons"),
+  });
   // convert react-native to react-native-web for storybook
   config.resolve.alias["react-native$"] = require.resolve("react-native-web");
+  // config.resolve.alias["react-native-vector-icons/"] = require.resolve("react-native-vector-icons/dist/");
 
   config.resolve.extensions.push(".js", ".jsx", ".mdx");
   return config;
